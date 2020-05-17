148a14a183916fedf44073aeb27506740d7d1177
Bart Hertog <Bart@EmbeddedAMS.nl>
2020-05-17 21:24:05 +0200

    Setup the project to include all the correct folders.

--- nucleo-f446re/Core/Src/main.cpp
+++ nucleo-f446re/Core/Src/main.cpp
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "uart_messages.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
